2b386d490581cdd32ff29ad3334e56d271d57a01
blurrcat <blurrcat@gmail.com>
2024-12-07 21:52:44 -0800

    day6

--- 2024/day4.ml
+++ 2024/day4.ml
@@ -5,23 +5,31 @@ module Graph : sig
     type t = int * int
 
     val move : t -> t -> t
+    val compare : t -> t -> int
+    val equal : t -> t -> bool
+    val pp : Format.formatter -> t -> unit
   end
 
-  type t
+  type 'c t
 
   val directions : Index.t list
-  val of_strings : string list -> t
-  val fold : ('a -> Index.t -> 'a) -> 'a -> t -> 'a
-  val get : t -> Index.t -> char
-  val get_path : t -> Index.t -> Index.t -> int -> char list
+  val of_strings : string list -> char t
+  val foldi : ('a -> Index.t -> 'c -> 'a) -> 'a -> 'c t -> 'a
+  val find_opt : ('c -> bool) -> 'c t -> (Index.t * 'c) option
+  val get : 'c t -> Index.t -> 'c
+  val get_opt : 'c t -> Index.t -> 'c option
+  val get_path : 'c t -> Index.t -> Index.t -> int -> 'c list
 end = struct
   module Index = struct
     type t = int * int
 
     let move index delta = Pair.map_same2 Int.add index delta
+    let equal = Pair.equal Int.equal Int.equal
+    let compare = Pair.compare Int.compare Int.compare
+    let pp = Pair.pp Int.pp Int.pp
   end
 
-  type t = { data : char array array; size : int * int }
+  type 'c t = { data : 'c array array; size : int * int }
 
   let of_strings ss =
     let sx = List.length ss in
@@ -32,24 +40,31 @@ end = struct
   let directions =
     [ (-1, -1); (-1, 0); (-1, 1); (0, 1); (0, -1); (1, -1); (1, 0); (1, 1) ]
 
-  let iteri : (Index.t -> unit) -> t -> unit =
-   fun f { size = sx, sy; _ } ->
-    for i = 0 to sx - 1 do
-      for j = 0 to sy - 1 do
-        f (i, j)
-      done
-    done
-
-  let fold f initial g =
-    let acc = ref initial in
-    iteri (fun idx -> acc := f !acc idx) g;
-    !acc
-
   let is_valid_index g (i, j) =
     let sx, sy = g.size in
     i >= 0 && i < sx && j >= 0 && j < sy
 
   let get g (i, j) = g.data.(i).(j)
+  let get_opt g idx = if is_valid_index g idx then Some (get g idx) else None
+
+  let iteri f g =
+    let sx, sy = g.size in
+    for i = 0 to sx - 1 do
+      for j = 0 to sy - 1 do
+        f (i, j) (get g (i, j))
+      done
+    done
+
+  let foldi f initial g =
+    let acc = ref initial in
+    iteri (fun idx c -> acc := f !acc idx c) g;
+    !acc
+
+  let find_opt test =
+    foldi
+      (fun r idx c ->
+        if Option.is_some r then r else if test c then Some (idx, c) else None)
+      None
 
   let get_path g idx (di, dj) n =
     List.range 0 (n - 1)
@@ -60,9 +75,9 @@ end
 
 let part1 lines =
   let g = Graph.of_strings lines in
-  Graph.fold
-    (fun acc idx ->
-      match Graph.get g idx with
+  Graph.foldi
+    (fun acc idx c ->
+      match c with
       | 'X' ->
           let matches =
             Graph.directions
@@ -102,9 +117,9 @@ MXMXAXMASX|}
 
 let part2 lines =
   let g = Graph.of_strings lines in
-  Graph.fold
-    (fun acc idx ->
-      match Graph.get g idx with
+  Graph.foldi
+    (fun acc idx c ->
+      match c with
       | 'A' ->
           let count_mas =
             (* delta to the corners relative to 'A' *)
---
+++ 2024/day6.ml
@@ -0,0 +1,100 @@
+open Containers
+module Graph = Day4.Graph
+module IndexSet = Set.Make (Graph.Index)
+
+let turn_right dir =
+  match dir with
+  | '^' -> '>'
+  | 'v' -> '<'
+  | '<' -> '^'
+  | '>' -> 'v'
+  | _ -> failwith ("unexpected direction: " ^ Char.to_string dir)
+
+let find_start map =
+  Graph.find_opt (fun c -> List.mem ~eq:Char.equal c [ '^'; 'v'; '>'; '<' ]) map
+  |> Option.get_exn_or "not found"
+
+let get_next_pos pos dir =
+  let delta =
+    match dir with
+    | '^' -> (-1, 0)
+    | 'v' -> (1, 0)
+    | '<' -> (0, -1)
+    | '>' -> (0, 1)
+    | _ -> (0, 0)
+  in
+  Graph.Index.move pos delta
+
+let part1 input =
+  let map = input |> String.lines |> Graph.of_strings in
+  let start_pos, start_dir = find_start map in
+  let rec walk : IndexSet.t -> Graph.Index.t -> char -> int =
+   fun path pos dir ->
+    let path = IndexSet.add pos path in
+    let next_pos = get_next_pos pos dir in
+    match Graph.get_opt map next_pos with
+    | Some '.' | Some '^' -> walk path next_pos dir
+    | Some '#' -> walk path pos (turn_right dir)
+    | Some _ -> failwith "unexpected tile"
+    | None -> path |> IndexSet.to_list |> List.length
+  in
+  walk IndexSet.empty start_pos start_dir
+
+module IndexDirSet = Set.Make (struct
+  type t = Graph.Index.t * char
+
+  let compare = Pair.compare Graph.Index.compare Char.compare
+end)
+
+let part2 input =
+  let map = input |> String.lines |> Graph.of_strings in
+  let start_pos, start_dir = find_start map in
+  Graph.foldi
+    (fun total new_obstruction _ ->
+      let get_opt p =
+        if Graph.Index.equal p new_obstruction then Some '#'
+        else Graph.get_opt map p
+      in
+      let rec is_loop path pos dir =
+        if IndexDirSet.mem (pos, dir) path then true
+        else
+          let path' = IndexDirSet.add (pos, dir) path in
+          let next_pos = get_next_pos pos dir in
+          match get_opt next_pos with
+          | Some '.' | Some '^' -> is_loop path' next_pos dir
+          | Some '#' -> is_loop path' pos (turn_right dir)
+          | Some tile -> failwith ("unexpected tile" ^ Char.to_string tile)
+          | None -> false
+      in
+      if is_loop IndexDirSet.empty start_pos start_dir then total + 1 else total)
+    0 map
+
+let test =
+  {|....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...|}
+
+let%expect_test "part1" =
+  part1 test |> Printf.printf "%d";
+  [%expect {|
+  41 |}];
+
+  Util.with_data_file "day6.txt" (fun ic ->
+      IO.read_all ic |> part1 |> Printf.printf "%d");
+  [%expect {| 4647 |}]
+
+let%expect_test "part2" =
+  part2 test |> Printf.printf "%d";
+  [%expect {|
+  6 |}];
+
+  Util.with_data_file "day6.txt" (fun ic ->
+      IO.read_all ic |> part2 |> Printf.printf "%d");
+  [%expect {| 1723 |}]
