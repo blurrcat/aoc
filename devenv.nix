6525483ff5c8353c52d6f29d34a943322778a7bc
blurrcat <blurrcat@gmail.com>
2024-12-15 10:34:23 -0800

    improve day7

--- devenv.nix
+++ devenv.nix
@@ -23,7 +23,7 @@ in
   scripts.run-perf.exec = ''
     dune clean
     dune build --profile release
-    perf record --call-graph dwarf 500 dune runtest
+    perf record --call-graph dwarf -F 500 dune runtest --profile release
     perf script | inferno-collapse-perf | inferno-flamegraph > perf.flamegraph.svg
     echo "open perf.flamegraph.svg"
   '';
